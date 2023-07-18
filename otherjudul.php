ac0415e1cfa5b4ca06e2efc481725fef99770e09
TenSkyline <72663408+TenSkyline@users.noreply.github.com>
2023-07-18 12:53:13 +0700

    Rayvaldo-otherjudul.php

--- otherjudul.php
+++ otherjudul.php
@@ -3,9 +3,9 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>APlikasi Other Penawaran</title>
+	<title>Aplikasi Other Penawaran</title>
 </head>
 <body>
-	<p align="center">Contoh Judul</p>
+	<p align="center">Contoh Judul By Rayvaldo</p>
 </body>
 </html>
